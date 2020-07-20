3fcb88ca1de85cdc62ea5cfeccf4a526351670f0
Surajv <vsuraj2405@gmail.com>
2020-07-20 20:47:21 +0530

    added example.js
    added example for ECMAScript 2016+ feature

---
+++ src/js/examples/example.js
@@ -0,0 +1,9 @@
+// Example for ECMAScripts 2016+ version
+
+// A simple replacement for Math.pow function is the exponentiation operator(**).
+
+// without exponentiation operator
+console.log(Math.pow(4, 3));   // ANS = 4*4*4 = 64
+
+// with exponentiation operator
+console.log(4 ** 3);   // ANS = 64
